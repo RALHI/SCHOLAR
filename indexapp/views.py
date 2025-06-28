b99711573c8df69203df84f95733e4a690e31d3f
RALHI <ralhi.certification@gmail.com>
2025-06-29 04:02:50 +0530

    render the service-section dynamically by passing the data from view.py->index.html->service-section.html

--- indexapp/views.py
+++ indexapp/views.py
@@ -9,8 +9,26 @@ nav_buttons = {
   "contact" : "Register Now",
 }
 
+services = [
+  {
+    "servicename" : "Online Degrees",
+    "content" : "Whenever you need free templates in HTML CSS, you just remember TemplateMo website."
+  },
+  {
+    "servicename" : "Short Courses",
+    "content" : "You can browse free templates based on different tags such as digital marketing, etc."
+  },
+  {
+    "servicename" : "Web Experts",
+    "content" : "You can start learning HTML CSS by modifying free templates from our website too."
+  },
+]
+
 def index(request):
   data = {
-    "nav_items" : nav_buttons
+    "nav_items" : nav_buttons,
+    "service" : services
   }
+
+  
   return render(request, "indexapp/index.html", data)
