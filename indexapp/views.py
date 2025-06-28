3df0eeabd2ec2b861d1b1556907af6eac9872d89
RALHI <ralhi.certification@gmail.com>
2025-06-29 04:21:25 +0530

    dynamically render the banner content by oassing data from views.py to main0banner.html

--- indexapp/views.py
+++ indexapp/views.py
@@ -24,11 +24,31 @@ services = [
   },
 ]
 
+banner_content = [
+  {
+    "category" : "Our Courses",
+    "heading" : "With Scholar Teachers, Everything Is Easier",
+    "description" : "Scholar is free CSS template designed by TemplateMo for online educational related websites. This layout is based on the famous Bootstrap v5.3.0 framework.",
+    "text" : "What's Scholar?"
+  },  
+  {
+    "category" : "Best Result",
+    "heading" : "Get the best result out of your effort",
+    "description" : "You are allowed to use this template for any educational or commercial purpose. You are not allowed to re-distribute the template ZIP file on any other website.",
+    "text" : "What's the best result?"
+  },  
+  {
+    "category" : "Online Learning",
+    "heading" : "Online Learning helps you save the time",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporious incididunt ut labore et dolore magna aliqua suspendisse.",
+    "text" : "What's Online Course?"
+  },  
+]
+
 def index(request):
   data = {
     "nav_items" : nav_buttons,
-    "service" : services
+    "banner" : banner_content,
+    "service" : services,
   }
-
-  
   return render(request, "indexapp/index.html", data)
